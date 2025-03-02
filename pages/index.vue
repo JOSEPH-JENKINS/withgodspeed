33ab37221b937116998e2e1efd0289632c2d28ac
Joseph Jenkins <amjosephjenkins@gmail.com>
2025-03-02 21:47:09 +0100

    messaging updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,10 @@
-<template></template>
+<template>
+  <div class="container-grid">
+    <h2>
+      We're all in on your growth. Customers choose you - and keep choosing you.
+    </h2>
+    <h2>
+      You're our top priority. That's why we take only 4 clients per quarter.
+    </h2>
+  </div>
+</template>

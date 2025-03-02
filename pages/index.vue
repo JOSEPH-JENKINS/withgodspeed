96256ef27b6917366ca6abb4f2e70dcd565e75e0
Joseph Jenkins <amjosephjenkins@gmail.com>
2025-03-02 21:53:23 +0100

    animations updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container-grid">
-    <h2>
+    <h2 class="animate-text">
       We're all in on your growth. Customers choose you - and keep choosing you.
     </h2>
-    <h2>
+    <h2 class="animate-text">
       You're our top priority. That's why we take only 4 clients per quarter.
     </h2>
   </div>
